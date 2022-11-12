07fb9f4aab3a1d8b9a0cde95cb0027c144cc45c9
Ankush469 <yadavankush469@gmail.com>
2022-11-12 23:14:02 +0530

    wellness

--- wellnesh.js
+++ wellnesh.js
@@ -3,43 +3,54 @@
 let data=[
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/1045847/lizol_disinfectant_surface_floor_cleaner_liquid_lavender_2_litre_416520_0_0.jpg",
-        name:"Lizol Disnfectant surface & floor"
+        name:"Lizol Disnfectant surface & floor",
+        price:"Best price Rs.140"
+       
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/1058752/the_body_shop_satsuma_shower_gel_250_ml_499917_0_1.jpg",
-        name:"The Body Shop Satsuma Shower Gel"
+        name:"The Body Shop Satsuma Shower Gel",
+        price:"Best price Rs.120"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/346480/tvaksh_face_care_face_wash_60_gm_0_1.jpg",
-        name:"tvaksh Face care Face Wash 60 gm"
+        name:"tvaksh Face care Face Wash 60 gm",
+        price:"Best price Rs.130"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
-        name:"Scalpe Plus Anti Dandruff Shampoo"
+        name:"Scalpe Plus Anti Dandruff Shampoo",
+        price:"Best price Rs.190"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
-        name:"La shield Pollution Protect Milneral"
+        name:"La shield Pollution Protect Milneral",
+        price:"Best price Rs.200"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/914707/dettol_instant_hand_sanitizer_original_500_ml_0_1.jpg",
-        name:"Dettol instant hand senitizer-original"
+        name:"Dettol instant hand senitizer-original",
+        price:"Best price Rs.300"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/824822/mamaearth_milky_soft_baby_face_cream_with_muru_muru_butter_50_gm_0.jpg",
-        name:"mamaearth milky soft face cream"
+        name:"mamaearth milky soft face cream",
+        price:"Best price Rs.600"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/889428/aroma_magic_face_wash_neem_tea_tree_100_ml_0.jpg",
-        name:"Aroma Madic Face Wash-neem & tea"
+        name:"Aroma Madic Face Wash-neem & tea",
+        price:"Best price Rs.100"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/853397/boro_plus_doodh_kesar_body_lotion_100_ml_0_1.jpg",
         name:"Boro plus Doodh keser Body Lotion",
+        price:"Best price Rs.930"
     },
     {
         image:"https://www.netmeds.com/images/product-v1/150x150/382418/cetaphil_dam_lotion_30gm_0.jpg",
-        name:"Cetaphil Dam Lotion 30gm"
+        name:"Cetaphil Dam Lotion 30gm",
+        price:"Best price Rs.740"
     }
 ]
 
@@ -58,12 +69,21 @@ const appent =(data)=>{
         let name=document.createElement("h3");
         name.innerText=el.name;
 
-        div.append(image,name);
+        let price=document.createElement("h4");
+        price.innerText=el.price;
+
+        let btn=document.createElement("button");
+        btn.innerText="ADD TO CART";
+        btn.setAttribute("class","btn")
+
+
+        div.append(image,name,price,btn);
         under.append(div);
     })
 }
 
 appent(data)
+
 
 let btnleft=document.getElementById("slide-left");
 btnleft.addEventListener("click",()=>{
@@ -75,3 +95,284 @@ btnright.addEventListener("click",()=>{
     slider.scrollLeft+=550;
     // btnright.style.transition= .54;
 })
+/////////////////////////////////////////////////
+let data_1=[
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/1045847/lizol_disinfectant_surface_floor_cleaner_liquid_lavender_2_litre_416520_0_0.jpg",
+        name:"Lizol Disnfectant surface & floor",
+        price:"Best price Rs.140"
+       
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/1058752/the_body_shop_satsuma_shower_gel_250_ml_499917_0_1.jpg",
+        name:"The Body Shop Satsuma Shower Gel",
+        price:"Best price Rs.120"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/346480/tvaksh_face_care_face_wash_60_gm_0_1.jpg",
+        name:"tvaksh Face care Face Wash 60 gm",
+        price:"Best price Rs.130"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
+        name:"Scalpe Plus Anti Dandruff Shampoo",
+        price:"Best price Rs.190"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
+        name:"La shield Pollution Protect Milneral",
+        price:"Best price Rs.200"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/914707/dettol_instant_hand_sanitizer_original_500_ml_0_1.jpg",
+        name:"Dettol instant hand senitizer-original",
+        price:"Best price Rs.300"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/824822/mamaearth_milky_soft_baby_face_cream_with_muru_muru_butter_50_gm_0.jpg",
+        name:"mamaearth milky soft face cream",
+        price:"Best price Rs.600"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/889428/aroma_magic_face_wash_neem_tea_tree_100_ml_0.jpg",
+        name:"Aroma Madic Face Wash-neem & tea",
+        price:"Best price Rs.100"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/853397/boro_plus_doodh_kesar_body_lotion_100_ml_0_1.jpg",
+        name:"Boro plus Doodh keser Body Lotion",
+        price:"Best price Rs.930"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/382418/cetaphil_dam_lotion_30gm_0.jpg",
+        name:"Cetaphil Dam Lotion 30gm",
+        price:"Best price Rs.740"
+    }
+]
+let under1=document.getElementById("slider1")
+const appent1 =(data_1)=>{
+
+    data_1.forEach((el)=>{
+
+        let div=document.createElement("div");
+        div.setAttribute("class","thumbnail1");
+
+        let image=document.createElement("img");
+        image.src=el.image;
+
+        let name=document.createElement("h3");
+        name.innerText=el.name;
+
+        let price=document.createElement("h4");
+        price.innerText=el.price;
+
+        let btn=document.createElement("button");
+        btn.innerText="ADD TO CART";
+        btn.setAttribute("class","btn")
+
+        div.append(image,name,price,btn);
+        under1.append(div);
+    })
+}
+
+appent1(data_1)
+
+
+let btnleft1=document.getElementById("slide-left1");
+btnleft1.addEventListener("click",()=>{
+    slider1.scrollLeft-=550;
+})
+
+let btnright1=document.getElementById("slide-right1");
+btnright1.addEventListener("click",()=>{
+    slider1.scrollLeft+=550;
+})
+
+// ------------------------------------------------------------
+
+let data_2=[
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/1045847/lizol_disinfectant_surface_floor_cleaner_liquid_lavender_2_litre_416520_0_0.jpg",
+        name:"Lizol Disnfectant surface & floor",
+        price:"Best price Rs.140"
+       
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/1058752/the_body_shop_satsuma_shower_gel_250_ml_499917_0_1.jpg",
+        name:"The Body Shop Satsuma Shower Gel",
+        price:"Best price Rs.120"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/346480/tvaksh_face_care_face_wash_60_gm_0_1.jpg",
+        name:"tvaksh Face care Face Wash 60 gm",
+        price:"Best price Rs.130"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
+        name:"Scalpe Plus Anti Dandruff Shampoo",
+        price:"Best price Rs.190"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
+        name:"La shield Pollution Protect Milneral",
+        price:"Best price Rs.200"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/914707/dettol_instant_hand_sanitizer_original_500_ml_0_1.jpg",
+        name:"Dettol instant hand senitizer-original",
+        price:"Best price Rs.300"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/824822/mamaearth_milky_soft_baby_face_cream_with_muru_muru_butter_50_gm_0.jpg",
+        name:"mamaearth milky soft face cream",
+        price:"Best price Rs.600"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/889428/aroma_magic_face_wash_neem_tea_tree_100_ml_0.jpg",
+        name:"Aroma Madic Face Wash-neem & tea",
+        price:"Best price Rs.100"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/853397/boro_plus_doodh_kesar_body_lotion_100_ml_0_1.jpg",
+        name:"Boro plus Doodh keser Body Lotion",
+        price:"Best price Rs.930"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/382418/cetaphil_dam_lotion_30gm_0.jpg",
+        name:"Cetaphil Dam Lotion 30gm",
+        price:"Best price Rs.740"
+    }
+]
+let under2=document.getElementById("slider2")
+const appent2 =(data_2)=>{
+
+    data_2.forEach((el)=>{
+
+        let div=document.createElement("div");
+        div.setAttribute("class","thumbnail2");
+
+        let image=document.createElement("img");
+        image.src=el.image;
+
+        let name=document.createElement("h3");
+        name.innerText=el.name;
+
+        let price=document.createElement("h4");
+        price.innerText=el.price;
+
+        let btn=document.createElement("button");
+        btn.innerText="ADD TO CART";
+        btn.setAttribute("class","btn")
+
+        div.append(image,name,price,btn);
+        under2.append(div);
+    })
+}
+
+appent2(data_2)
+
+
+let btnleft2=document.getElementById("slide-left2");
+btnleft2.addEventListener("click",()=>{
+    slider2.scrollLeft-=550;
+});
+
+let btnright2=document.getElementById("slide-right2");
+btnright2.addEventListener("click",()=>{
+    slider2.scrollLeft+=550;
+});
+
+
+// --------------------------------------------------------------------------
+
+let data_3=[
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/1045847/lizol_disinfectant_surface_floor_cleaner_liquid_lavender_2_litre_416520_0_0.jpg",
+        name:"Lizol Disnfectant surface & floor",
+        price:"Best price Rs.140"
+       
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/1058752/the_body_shop_satsuma_shower_gel_250_ml_499917_0_1.jpg",
+        name:"The Body Shop Satsuma Shower Gel",
+        price:"Best price Rs.120"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/346480/tvaksh_face_care_face_wash_60_gm_0_1.jpg",
+        name:"tvaksh Face care Face Wash 60 gm",
+        price:"Best price Rs.130"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
+        name:"Scalpe Plus Anti Dandruff Shampoo",
+        price:"Best price Rs.190"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/952462/la_shield_pollution_protect_mineral_sunscreen_gel_spf_50_50_gm_0_0.jpg",
+        name:"La shield Pollution Protect Milneral",
+        price:"Best price Rs.200"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/914707/dettol_instant_hand_sanitizer_original_500_ml_0_1.jpg",
+        name:"Dettol instant hand senitizer-original",
+        price:"Best price Rs.300"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/824822/mamaearth_milky_soft_baby_face_cream_with_muru_muru_butter_50_gm_0.jpg",
+        name:"mamaearth milky soft face cream",
+        price:"Best price Rs.600"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/889428/aroma_magic_face_wash_neem_tea_tree_100_ml_0.jpg",
+        name:"Aroma Madic Face Wash-neem & tea",
+        price:"Best price Rs.100"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/853397/boro_plus_doodh_kesar_body_lotion_100_ml_0_1.jpg",
+        name:"Boro plus Doodh keser Body Lotion",
+        price:"Best price Rs.930"
+    },
+    {
+        image:"https://www.netmeds.com/images/product-v1/150x150/382418/cetaphil_dam_lotion_30gm_0.jpg",
+        name:"Cetaphil Dam Lotion 30gm",
+        price:"Best price Rs.740"
+    }
+]
+let under3=document.getElementById("slider3")
+const appent3 =(data_3)=>{
+
+    data_3.forEach((el)=>{
+
+        let div=document.createElement("div");
+        div.setAttribute("class","thumbnail3");
+
+        let image=document.createElement("img");
+        image.src=el.image;
+
+        let name=document.createElement("h3");
+        name.innerText=el.name;
+
+        let price=document.createElement("h4");
+        price.innerText=el.price;
+
+        let btn=document.createElement("button");
+        btn.innerText="ADD TO CART";
+        btn.setAttribute("class","btn")
+
+        div.append(image,name,price,btn);
+        under3.append(div);
+    })
+}
+
+appent3(data_3)
+
+
+let btnleft3=document.getElementById("slide-left3");
+btnleft3.addEventListener("click",()=>{
+    slider3.scrollLeft-=550;
+});
+
+let btnright3=document.getElementById("slide-right3");
+btnright3.addEventListener("click",()=>{
+    slider3.scrollLeft+=550;
+});
